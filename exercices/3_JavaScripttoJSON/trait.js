50db419149b7c57053d5ba42e423b3f8db4f9baa
Unknown <benjamin.delacombaz@cpnv.ch>
2018-01-17 09:22:30 +0100

    Various correct and Exercice 3 finish

---
+++ exercices/3_JavaScripttoJSON/trait.js
@@ -0,0 +1,14 @@
+function showJSON()
+{
+    var obj = {"employees":[{"firstName":"John", "lastName":"Doe"},{"firstName":"Anna", "lastName":"Smith"},{"firstName":"Peter", "lastName":"Jones"}]};
+    var json = convertJavaScripttoJSON(obj);
+    document.getElementById('divJSON').innerHTML = json;
+}
+
+function convertJavaScripttoJSON(obj)
+{
+    var json = null;
+    // Ajoutez la fonction permettant de convertir l'objet JavaScript en JSON
+    
+    return json;
+}

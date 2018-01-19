9b2fabecd30e41fe86df45f15049cbb75cc160d2
Unknown <benjamin.delacombaz@cpnv.ch>
2018-01-19 09:11:16 +0100

    Update demo

--- demo/trait.js
+++ demo/trait.js
@@ -2,7 +2,9 @@ function showObj()
 {
     var json = '{"course":{"voiture":[{"marque":"Ford","modele":"GT40","pilotes":{"pilote":["Bastien","Nicolas"]}},{"marque":"Ferrari","modele":"250 GTO","pilotes":{"pilote":["Benjamin","Kevin"]}}]}}';
     var obj = convertJSONtoJavascript(json);
-    document.getElementById('divObj').innerHTML = obj.course.voiture[0].marque;
+    var voiture1 = obj.course.voiture[0].marque + " " + obj.course.voiture[0].modele + " conduite par " + obj.course.voiture[0].pilotes.pilote[0] + " et " + obj.course.voiture[0].pilotes.pilote[1];
+    var voiture2 = obj.course.voiture[1].marque + " " + obj.course.voiture[1].modele + " conduite par " + obj.course.voiture[1].pilotes.pilote[0] + " et " + obj.course.voiture[1].pilotes.pilote[1]
+    document.getElementById('divObj').innerHTML = voiture1 + "</br>" + voiture2;
 }
 
 function convertJSONtoJavascript(jsonString)
